ad8426d9bbce590575b068c7d0cafdf8a5f30ded
githmi-ruchirangi <githmiruchirangi@gmail.com>
2024-07-22 23:56:40 +0530

    disable download cv buttont

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import TypeWriteEffect from "./Helper/TypeWriteEffect";
 import Image from 'next/image';
 
-
 interface Props {
     contactRef: React.RefObject<HTMLElement>;
 }
@@ -39,7 +38,6 @@ const Hero = ({ contactRef }: Props) => {
         }
     };
 
-
     useEffect(() => {
         const hiremeButton = document.getElementById('hire-me-button');
         if (hiremeButton) {
@@ -53,61 +51,49 @@ const Hero = ({ contactRef }: Props) => {
         };
     }, []);
     
-
-    
-    // const handleDownloadCV = () => {
-    //     const cvUrl = "https://example.com/path/to/cv.pdf"; 
-    //     window.open(cvUrl, "_blank");
-    // };
-
-    
     return (
-    <div className="w-[100vw] pt-[4vh] md:pt-[12vh] h-[85vh] md:h-[100vh] custom-bg">
-       <div className="flex justify-center flex-col w-[80%] h-[100%] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[3rem] items-center">
-        <div>
-            <h1 data-aos="fade-right" className="text-[#c4cfde] mb-[1.3rem]">WELCOME TO MY WORLD</h1>
-            <div data-aos="fade-left" data-aos-delay="400" >
-                <h1 className="x1:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white">
-                    Hi, I'm <span className="text-yellow-300">Wihanga</span>
-                </h1>
-                <TypeWriteEffect/>
-        </div>
-        <p 
-        data-aos="fade-up" 
-        data-aos-delay="800"  
-        className="mt-[1.3rem] text-[15px] md:text-[13px] text-[#c4cfde] text-justify">
-            A passionate software engineer with over one year of experience in Java, Spring Boot and Microservices, specializing in backend development and REST APIs. Additionally, I have over a year of experience in React, Python and Flask. I excel at problem-solving, system design, and system enhancement. As a responsible and self-motivated individual, I am a recent graduate with strong technical skills, eager to embrace challenges, and dedicated to utilizing my abilities to contribute to organizational growth.</p>
-             <div className="mt-[2rem] flex items-center space-x-6">
-                <button 
-                data-aos="zoom-in" 
-                data-aos-delay="1200" 
-                id="hire-me-button"
-                className=" relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-700 text-white
-                shadow-2xl transition-all rounded-md hover:-translate-y-1 hover:scale-105 before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out
-                hover:before:h-56 hover:before:w-56">
-                    <span className="relative z-10">Hire me</span>
-                </button>
-                <button 
-                data-aos="zoom-out" 
-                data-aos-delay="1600" 
-                id="download-cv-button"
-                className="before:ease relative h-12 w-40 overflow-hidden bg-green-800 text-white font-semibold rounded-md hover:-translate-y-1 hover:scale-105 shadow-2xl before:absolute before:left-0 before:-ml-2 
-                before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-green-600 before:transition-all before:duration-300 
-                hover:text-white hover:shadow-black hover:before:-rotate-180 ">
-                    <span className="relative z-10">Download CV</span>
-                </button>
-             </div>
-        </div>
-        <div 
-        // data-aos="fade-left" 
-        // data-aos-delay="2000" 
-        className="hidden md:block">
-        <Image src="/images/hero.svg" alt="hero" width={600} height={600} className="object-contain"/>
-        </div>
-        </div>
-       </div>
+        <div className="w-[100vw] pt-[4vh] md:pt-[12vh] h-[85vh] md:h-[100vh] custom-bg">
+            <div className="flex justify-center flex-col w-[80%] h-[100%] mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-[3rem] items-center">
+                    <div>
+                        <h1 data-aos="fade-right" className="text-[#c4cfde] mb-[1.3rem]">WELCOME TO MY WORLD</h1>
+                        <div data-aos="fade-left" data-aos-delay="400" >
+                            <h1 className="x1:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white">
+                                Hi, I'm <span className="text-yellow-300">Wihanga</span>
+                            </h1>
+                            <TypeWriteEffect/>
+                        </div>
+                        <p 
+                            data-aos="fade-up" 
+                            data-aos-delay="800"  
+                            className="mt-[1.3rem] text-[15px] md:text-[13px] text-[#c4cfde] text-justify">
+                            A passionate software engineer with over one year of experience in Java, Spring Boot and Microservices, specializing in backend development and REST APIs. Additionally, I have over a year of experience in React, Python and Flask. I excel at problem-solving, system design, and system enhancement. As a responsible and self-motivated individual, I am a recent graduate with strong technical skills, eager to embrace challenges, and dedicated to utilizing my abilities to contribute to organizational growth.
+                        </p>
+                        <div className="mt-[2rem] flex items-center space-x-6">
+                            <button 
+                                data-aos="zoom-in" 
+                                data-aos-delay="1200" 
+                                id="hire-me-button"
+                                className="relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-700 text-white shadow-2xl transition-all rounded-md hover:-translate-y-1 hover:scale-105 before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56">
+                                <span className="relative z-10">Hire me</span>
+                            </button>
+                            <button 
+                                data-aos="zoom-out" 
+                                data-aos-delay="1600" 
+                                id="download-cv-button"
+                                className="relative h-12 w-40 overflow-hidden bg-gray-400 text-gray-700 font-semibold rounded-md cursor-not-allowed"
+                                disabled>
+                                <span className="relative z-10">Download CV</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="hidden md:block">
+                        <Image src="/images/hero.svg" alt="hero" width={600} height={600} className="object-contain"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
-export default Hero;
+
+export default Hero;
